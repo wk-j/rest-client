eee94158748a769b4f5bfd16b5db2dd1d3d94864
wk <somnuk.wk@gmail.com>
2021-01-24 01:21:52 +0700

    Process get

--- src/RestClient/Program.cs
+++ src/RestClient/Program.cs
@@ -3,22 +3,55 @@ using System.Collections.Generic;
 
 using static RestClient.RequestParser;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text.Json;
 
 namespace RestClient {
 
     class Program {
-        static void Main(string[] args) {
-            var r = File.ReadAllText("http/register.rest");
-            var result = XRequest(r);
 
-            Console.WriteLine(result.Method);
-            Console.WriteLine(result.Url);
+        static async Task ProcessGet(HttpClient client, string url) {
+            var result = await client.GetAsync(url);
+            var body = await result.Content.ReadAsStringAsync();
 
-            foreach (var item in result.Headers) {
-                Console.WriteLine($" H {item.Key}:{item.Value}");
+            var headers = result.Headers.GetEnumerator();
+            while (headers.MoveNext()) {
+                var current = headers.Current;
+                Console.WriteLine($"{current.Key}: {string.Join(", ", current.Value)}");
             }
 
-            Console.WriteLine(result.Body);
+            var c = result.Content.Headers.GetEnumerator();
+            while (c.MoveNext()) {
+                var current = c.Current;
+                Console.WriteLine($"{current.Key}: {string.Join(", ", current.Value)}");
+            }
+
+            Console.WriteLine();
+
+            var ok = result.Content.Headers.TryGetValues("Content-Type", out var contentType);
+            if (ok && contentType.Any(x => x.Contains("application/json"))) {
+                var obj = JsonSerializer.Deserialize<dynamic>(body);
+                var json = JsonSerializer.Serialize(obj, new JsonSerializerOptions {
+                    WriteIndented = true
+                });
+                Console.WriteLine(json);
+            } else {
+                // Console.WriteLine(body);
+            }
+
+        }
+
+        static async Task Main(string[] args) {
+            // var r = File.ReadAllText("http/register.rest");
+            var r = File.ReadAllText("http/users.rest");
+            var request = XRequest(r);
+
+            var client = new HttpClient();
+            if (request.Method == Method.Get) {
+                await ProcessGet(client, request.Url);
+            }
         }
     }
 }
