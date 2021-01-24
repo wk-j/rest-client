6275efa2a27a2c54f2d86aa9c396a44845eae090
wk <somnuk.wk@gmail.com>
2021-01-24 13:19:45 +0700

    Process post

--- src/RestClient/Program.cs
+++ src/RestClient/Program.cs
@@ -7,15 +7,14 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Text.Json;
+using System.Text;
 
 namespace RestClient {
 
     class Program {
 
-        static async Task ProcessGet(HttpClient client, string url) {
-            var result = await client.GetAsync(url);
+        static async Task PrintResponse(HttpResponseMessage result) {
             var body = await result.Content.ReadAsStringAsync();
-
             var headers = result.Headers.GetEnumerator();
             while (headers.MoveNext()) {
                 var current = headers.Current;
@@ -38,19 +37,45 @@ namespace RestClient {
                 });
                 Console.WriteLine(json);
             } else {
-                // Console.WriteLine(body);
+                Console.WriteLine(body);
             }
 
         }
 
+        private static (bool, string) GetHeaderValue(Header[] headers, string key) {
+            var h = headers.FirstOrDefault(x => x.Key == key);
+            if (h == null) {
+                return (true, "text/plain");
+            }
+            return (true, h.Value);
+        }
+
+        private static async Task ProcessGet(HttpClient client, RequestInfo info) {
+            var url = info.Url;
+            var result = await client.GetAsync(url);
+            await PrintResponse(result);
+        }
+
+        private static async Task ProcessPost(HttpClient client, RequestInfo info) {
+            var url = info.Url;
+            var headers = info.Headers;
+            var body = info.Body;
+            var (ok, contentType) = GetHeaderValue(headers, "Content-Type");
+            var content = new StringContent(body, Encoding.UTF8, contentType);
+            var response = await client.PostAsync(url, content);
+            await PrintResponse(response);
+        }
+
         static async Task Main(string[] args) {
             // var r = File.ReadAllText("http/register.rest");
-            var r = File.ReadAllText("http/users.rest");
+            var r = File.ReadAllText("http/register.rest");
             var request = XRequest(r);
 
             var client = new HttpClient();
             if (request.Method == Method.Get) {
-                await ProcessGet(client, request.Url);
+                await ProcessGet(client, request);
+            } else if (request.Method == Method.Post) {
+                await ProcessPost(client, request);
             }
         }
     }
